88d4b438ce92bebf66d64d0fc2767af9db73c85d
snyderthing <107716018+snyderthing@users.noreply.github.com>
2026-05-13 19:05:08 -0700

    connect to database

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
+// PDO = PHP Data Object
+$dsn = 'mysql:host=localhost;port=3306;dbname=laracastsapp;user=root;password=Fl13gen$tein;charset=utf8mb4';
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare('SELECT * FROM posts');
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo '<li>' . $post['title'] . '</li>';
+}
