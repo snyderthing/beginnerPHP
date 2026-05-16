b90bad2f702ea6781b5113583fb8171699c271d4
snyderthing <107716018+snyderthing@users.noreply.github.com>
2026-05-15 23:31:00 -0700

    delete commented code

--- index.php
+++ index.php
@@ -5,11 +5,3 @@ require 'Database.php';
 require 'router.php';
 
 $config = require('config.php');
-
-
-// $id = $_GET['id'];
-// $query = "SELECT * FROM posts where id = :id";
-
-// $posts = $db->query($query, [':id' => $id])->fetch();
-
-// dd($posts);
