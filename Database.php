be28909dc8e8345c3ee10576abafae981ddf7e4a
snyderthing <107716018+snyderthing@users.noreply.github.com>
2026-05-16 11:04:12 -0700

    programming is rewriting

--- Database.php
+++ Database.php
@@ -4,6 +4,8 @@ class Database
 {
     public $connection;
 
+    public $statement;
+
     public function __construct($config, $username = 'root', $password = 'Fl13gen$tein')
     {
         $dsn = 'mysql:' . http_build_query($config, '', ';');
@@ -15,10 +17,36 @@ class Database
 
     public function query($query, $params = [])
     {
-        $statement = $this->connection->prepare($query);
+        $this->statement = $this->connection->prepare($query);
 
-        $statement->execute($params);
+        $this->statement->execute($params);
 
-        return $statement;
+        return $this;
+    }
+
+    public function get()
+    {
+        return $this->statement->fetchAll();
+    }
+
+public function find()
+    {
+        return $this->statement->fetch();
+    }
+
+    public function findOrFail()
+    {
+        $result = $this->find();
+
+        if (!$result) {
+            abort(Response::NOT_FOUND);
+        }
+
+        return $result;
+    }
+
+    public function fetchAll()
+    {
+        return $this->statement->fetchAll();
     }
 }
