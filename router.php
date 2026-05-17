5452eac7392be28bd08b3e5c55f143adbe25b4ee
snyderthing <107716018+snyderthing@users.noreply.github.com>
2026-05-17 16:01:13 -0700

    intro to forms and request methods

--- router.php
+++ router.php
@@ -1,14 +1,6 @@
 <?php
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/notes' => 'controllers/notes.php',
-    '/note' => 'controllers/note.php',
-    '/contact' => 'controllers/contact.php'
-];
+$routes = require 'routes.php';
 
 
 function routeToController($uri, $routes)
@@ -30,4 +22,6 @@ function abort($code = 404)
     die();
 }
 
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 routeToController($uri, $routes);
