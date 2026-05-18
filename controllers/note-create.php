bce36e034823346c3c41e196e49558726012872c
snyderthing <107716018+snyderthing@users.noreply.github.com>
2026-05-17 20:09:19 -0700

    always escape untrusted input

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,20 +6,23 @@ $db = new Database($config['database']);
 $heading = "Create Note";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query('INSERT INTO notes (body, user_id) VALUES (:body, :user_id)', [
-        'body' => $_POST['body'],
-        'user_id' => 1
-    ]);
-    // $config = require('config.php');
-    // $db = new Database($config['database']);
 
-    // $db->query("INSERT INTO notes (body, user_id) VALUES (:body, :user_id)", [
-    //     'body' => $_POST['body'],
-    //     'user_id' => 1
-    // ]);
+    $errors = [];
 
-    // header('Location: /notes');
-    // die();
+    if (strlen($_POST['body']) === 0) {
+        $errors['body'] = 'Note body is required';
+    }
+
+    if (strlen($_POST['body']) > 1000) {
+        $errors['body'] = 'Whoa! Easy, tiger!';
+    }
+
+    if (empty($errors)) {
+        $db->query('INSERT INTO notes (body, user_id) VALUES (:body, :user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]);
+    }
 }
 
 require 'views/note-create.view.php';
