eb0295dfc6b14c4046657aefc5941d570150fae2
snyderthing <107716018+snyderthing@users.noreply.github.com>
2026-05-18 15:01:59 -0700

    complete PHP autoloading and extraction including moving functions.php and router.php

--- Core/router.php
+++ Core/router.php
@@ -1,12 +1,9 @@
 <?php
 
-$routes = require 'routes.php';
-
-
 function routeToController($uri, $routes)
 {
     if (array_key_exists($uri, $routes)) {
-        require $routes[$uri];
+        require base_path($routes[$uri]);
     } else {
         abort();
     }
@@ -17,11 +14,12 @@ function abort($code = 404)
 {
     http_response_code($code);
 
-    require "views/{$code}.php";
+    require base_path("views/{$code}.php");
 
     die();
 }
 
-
+$routes = require base_path('routes.php');
+// Why is the above line at the top of the code page when the PHP Autoloading and Extraction video shows it at the bottom?
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 routeToController($uri, $routes);
--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -1,10 +1,9 @@
 <?php
 
-$config = require('config.php');
+$config = require base_path('config.php');
 $db = new Database($config['database']);
 
-$heading = "Note";
-    $currentUserId = 1;
+$currentUserId = 1;
 
 $note = $db->query("SELECT * FROM notes WHERE id = :id", [
     'id' => $_GET['id']
@@ -12,4 +11,7 @@ $note = $db->query("SELECT * FROM notes WHERE id = :id", [
 
 authorize($note['user_id'] === $currentUserId);
 
-require "views/notes/show.view.php";
+view("notes/show.view.php", [
+    'heading' => 'Note',
+    'note' => $note
+]);
