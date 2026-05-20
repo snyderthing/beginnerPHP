43491fe9e468d5e7749b9b1728b2f4ad5bbd09e6
snyderthing <107716018+snyderthing@users.noreply.github.com>
2026-05-19 18:27:59 -0700

    build a better router - beware of errors

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -5,14 +5,14 @@ use Core\Database;
 $config = require base_path('config.php');
 $db = new Database($config['database']);
 
-    $currentUserId = 25;
+$currentUserId = 1;
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $note = $db->query("SELECT * FROM notes WHERE id = :id", [
+    $note = $db->query("SELECT * FROM notes WHERE id = :id", [
         'id' => $_GET['id']
     ])->findOrFail();
 
-        authorize($note['user_id'] === $currentUserId);
+    authorize($note['user_id'] === $currentUserId);
 
     $db->query("DELETE FROM notes WHERE id = :id", [
         'id' => $_GET['id']
@@ -21,7 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     header('Location: /notes');
     exit();
 } else {
-            $note = $db->query("SELECT * FROM notes WHERE id = :id", [
+    $note = $db->query("SELECT * FROM notes WHERE id = :id", [
         'id' => $_GET['id']
     ])->findOrFail();
 
