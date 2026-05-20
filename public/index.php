2412684ee47a8ddbc2c64885c51a579661aac485
snyderthing <107716018+snyderthing@users.noreply.github.com>
2026-05-20 10:05:18 -0700

    make your first service container

--- public/index.php
+++ public/index.php
@@ -9,8 +9,9 @@ spl_autoload_register(function ($class) {
 require base_path("{$class}.php");
 });
 
-$router = new \Core\Router();
+require base_path('bootstrap.php');
 
+$router = new \Core\Router();
 $routes = require base_path('routes.php');
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
