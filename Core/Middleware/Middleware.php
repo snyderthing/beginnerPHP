3a15aa3f7821ea234e4aad37d7dbfd9f038bc846
snyderthing <107716018+snyderthing@users.noreply.github.com>
2026-05-23 16:52:49 -0700

    introduction to middleware

---
+++ Core/Middleware/Middleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Core\Middleware;
+
+class Middleware
+{
+    public const MAP = [
+        'guest' => Guest::class,
+        'auth' => Authenticated::class
+    ];
+
+    public static function resolve($key)
+    {
+        if (!$key) {
+            return;
+        }
+        $middleware = static::MAP[$key] ?? false;
+        if (! $middleware) {
+            throw new \Exception("No middleware found with key: {$key}");
+        }
+        (new $middleware)->handle();
+    }
+}
