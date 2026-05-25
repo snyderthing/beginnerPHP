85af943aae885a2dd75a367ad3ffdcaa59df8cee
snyderthing <107716018+snyderthing@users.noreply.github.com>
2026-05-24 18:58:56 -0700

    extract and authenticator class

--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -1,56 +1,21 @@
 <?php
 
-use Core\App;
-use Core\Database;
-use Core\Validator;
 use Http\Forms\LoginForm;
-
-// Log in the user if the credentials are correct
-$db = App::resolve(Database::class);
+use Core\Authenticator;
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
 $form = new LoginForm();
-if (! $form->validate($email, $password)) {
-    return view('session/create.view.php', [
-        'errors' => $form->errors()
-    ]);
-}
+if ($form->validate($email, $password)) {
 
-// $errors = [];
-// if (! Validator::email($email)) {
-//     $errors['email'] = 'A valid email address is required.';
-// }
-
-// if (! Validator::string($password)) {
-//     $errors['password'] = 'A valid password is required.';
-// }
-
-// if (! empty($errors)) {
-//     return view('session/create.view.php', [
-//         'errors' => $errors
-//     ]);
-// }
-
-// match the credentials
-$user = $db->query('SELECT * FROM users WHERE email = :email', [
-    'email' => $email
-])->find();
-
-if ($user) {
-    if (password_verify($password, $user['password'])) {
-        login([
-            'email' => $email
-            ]);
-
-        header('location: /');
-        exit();
+    if ((new Authenticator)->attempt($email, $password)) {
+        redirect('/');
     }
+    
+    $form->error('email', 'No user found with that email address and password.');
 }
 
 return view('session/create.view.php', [
-    'errors' => [
-        'email' => 'No user found with that email address and password.'
-    ]
+    'errors' => $form->errors()
 ]);
